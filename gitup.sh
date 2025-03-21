72746a7208e5852d05d09ceca8716a6f72c9f8d2
manupanand <manupanandindianinvestor@gmail.com>
2025-03-21 20:51:29 +0530

    test script

--- gitup.sh
+++ gitup.sh
@@ -3,3 +3,9 @@ echo commit for ?
 read commit
 git commit -m "$commit"
 git push origin main
+# install ansible core
+#sudo dnf install -y ansible-core.aarch64  2>&1 | tee -a /var/log/user_data.log
+
+# install setup 
+#ansible-pull -i localhost, -U https:github/repo/asniblerepo playbook.yaml  vault_token=${vault_token} 2>&1 | tee -a /var/log/user_data.log
+
--- tools-infra/terraform/infra/userdata.sh
+++ tools-infra/terraform/infra/userdata.sh
@@ -6,9 +6,3 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_c
 systemctl restart sshd
 
 
-# install ansible core
-#sudo dnf install -y ansible-core.aarch64  2>&1 | tee -a /var/log/user_data.log
-
-# install setup 
-#ansible-pull -i localhost, -U https:github/repo/asniblerepo playbook.yaml  vault_token=${vault_token} 2>&1 | tee -a /var/log/user_data.log
-
