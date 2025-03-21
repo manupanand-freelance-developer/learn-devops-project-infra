f57ce409625ea16c691c626327795ad644c5602b
manupanand <manupanandindianinvestor@gmail.com>
2025-03-21 23:47:37 +0530

    update script test

--- tools-infra/terraform/infra/userdata.sh
+++ tools-infra/terraform/infra/userdata.sh
@@ -1,9 +1,16 @@
 #!/bin/bash 
 # Set the password for the user "ec2-user" or any user you have
-echo "${AWS_USER}:${AWS_PASSWORD}" | chpasswd
-# Optionally enable password authentication if it's disabled by default
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart sshd
+echo "${AWS_USER}:${AWS_PASSWORD}" | chpasswd   2>&1 | tee -a /var/log/user_data.log
+
+
+# Enable password authentication in SSH
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config  2>&1 | tee -a /var/log/user_data.log
+
+sudo sed -i 's/^ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config  2>&1 | tee -a /var/log/user_data.log
+
+
+# Ensure UsePAM is enabled
+echo "UsePAM yes" | sudo tee -a /etc/ssh/sshd_config
 
 # install ansible core
 sudo dnf install -y ansible-core.aarch64  2>&1 | tee -a /var/log/user_data.log
