4b87765a7cbc5f944160feca5f06266aa38eb385
manupanand <manupanandindianinvestor@gmail.com>
2025-03-21 10:16:11 +0530

    infra script

--- tools-infra/terraform/infra/userdata.sh
+++ tools-infra/terraform/infra/userdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 # install ansible core
-# sudo dnf install -y ansible-core.aarch64  2>&1 | tee -a /var/log/user_data.log
+sudo dnf install -y ansible-core.aarch64  2>&1 | tee -a /var/log/user_data.log
 
-# # install setup 
-# ansible-pull -i localhost, -U https:github/repo/asniblerepo playbook.yaml  vault_token=${vault_token} 2>&1 | tee -a /var/log/user_data.log
+# install setup 
+ansible-pull -i localhost, -U https:github/repo/asniblerepo playbook.yaml  vault_token=${vault_token} 2>&1 | tee -a /var/log/user_data.log
 
 echo ${AWS_USER}
