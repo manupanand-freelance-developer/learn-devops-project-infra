56254379dec516e9247e1feb95aca9af18830c4d
manupanand <manupanandindianinvestor@gmail.com>
2025-03-22 14:19:15 +0530

    updaeted code for test

--- tools-infra/terraform/variable.tf
+++ tools-infra/terraform/variable.tf
@@ -7,29 +7,45 @@ variable "route53-zoneid" {
 variable "tools" {
   default = {
     # workstation={
-    #     instance_type=""
-    #     ports={}
-    #     policy_name=[""]
-    #     volume_size=
+    #     instance_type="t4g.large"  #t4g.micro  t4g.xlarge(4/16) t4g.large(2/8)
+    #     ports={https=443}
+    #     policy_name=["AmazonEC2FullAccess","AmazonSSMFullAccess","AmazonECRFullAccess","CloudWatchAgentServerPolicy","IAMPassRole","AmazonS3ReadOnlyAccess"]
+    #     volume_size= 25  
     #     create_dns= true 
-    #     create_iam_role=true
+    #     create_iam_role=true 
     # }
-    # github-runner={
-    #     instance_type=""
-    #     ports={}
-    #     policy_name=[""]
-    #     volume_size=
+    # grunner_arm={
+    #     instance_type="t4g.xlarge"  #c7g.xlarge(4/8)  t4g.xlarge(4/16)
+    #     ports={https=443}
+    #     policy_name=["AmazonEC2FullAccess","AmazonSSMFullAccess","AmazonECRFullAccess","CloudWatchAgentServerPolicy","IAMPassRole","AmazonS3ReadOnlyAccess" ]
+    #     volume_size= 25 
     #     create_dns= true 
-    #     create_iam_role=true
+    #     create_iam_role=true 
+    # }
+    # grunner_x86={
+    #     instance_type="c6i.xlarge"  #c6i.xlarge(4/8)   m6i.xlarge (4/16)
+    #     ports={https=443}
+    #     policy_name=["AmazonEC2FullAccess","AmazonSSMFullAccess","AmazonECRFullAccess","CloudWatchAgentServerPolicy","IAMPassRole","AmazonS3ReadOnlyAccess" ]
+    #     volume_size= 25
+    #     create_dns= true 
+    #     create_iam_role=true 
     # }
     test={
-        instance_type="t4g.small"  #c7g.xlarge  t4g.xlarge
+        instance_type="t4g.micro"  #c7g.medium(1/2) c7g.xlarge  t4g.xlarge  t4g.micro
         ports={https=443}
         policy_name=["AmazonEC2FullAccess","AmazonSSMFullAccess"]
         volume_size= 10  
         create_dns= true 
         create_iam_role=true 
     }
+    #   test_x86={
+    #     instance_type="t3.small"  #intel
+    #     ports={https=443}
+    #     policy_name=["AmazonEC2FullAccess","AmazonSSMFullAccess"]
+    #     volume_size= 10  
+    #     create_dns= true 
+    #     create_iam_role=true 
+    # }
     # vault={
     #     instance_type=""
     #     ports={ vault= 8200}
