49f03f27873e96e5a97ef054048f83a8be26f259
manupanand <manupanandindianinvestor@gmail.com>
2025-03-23 21:55:34 +0530

    remoev destroy

--- gitup.sh
+++ gitup.sh
@@ -8,12 +8,13 @@ git push origin main
 
 # install setup 
 #ansible-pull -i localhost, -U https:github/repo/asniblerepo playbook.yaml  vault_token=${vault_token} 2>&1 | tee -a /var/log/user_data.log
-
+BASE_DIR="/actions-runner/_work/learn-devops-project-infra/learn-devops-project-infra"
  for role in "bastion-node" "grunner_arm"; do
             echo "Destroying infrastructure for $role"
             export TF_VAR_role_name="$role"
             ls -la 
-            cd "$(pwd)/tools-infra/terraform"      # Always reset to the correct directory
+            cd "$BASE_DIR/tools-infra/terraform"      # Always reset to the correct directory
+            pwd
             ls -la 
             echo $role
             cd ..
