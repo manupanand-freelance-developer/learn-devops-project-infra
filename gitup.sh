f3441b4c3842b95fa4d5f98ca115ea54db503eb9
manupanand <manupanandindianinvestor@gmail.com>
2025-03-23 21:16:13 +0530

    running infra

--- gitup.sh
+++ gitup.sh
@@ -9,3 +9,10 @@ git push origin main
 # install setup 
 #ansible-pull -i localhost, -U https:github/repo/asniblerepo playbook.yaml  vault_token=${vault_token} 2>&1 | tee -a /var/log/user_data.log
 
+ for role in "bastion-node" "grunner_arm"; do
+            echo "Destroying infrastructure for $role"
+            export TF_VAR_role_name="$role"
+            ls -la 
+            # cd tools-infra/terraform
+            # terraform destroy -auto-approve
+          done
